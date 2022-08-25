0709fca665b1e8d71bff310c01d75130c402fcc6
Simon Zelenski <simon.zelenski@gmail.com>
2022-08-26 00:34:11 +0200

    it works!

--- app/go.mod
+++ app/go.mod
@@ -2,4 +2,8 @@ module main
 
 go 1.19
 
-require github.com/smnzlnsk/go-utils v0.0.0-20220825221306-f29c4fb305ed // indirect
+require github.com/smnzlnsk/go-utils v0.0.0-20220825221306-f29c4fb305ed // direct
+
+require golang.org/x/net v0.0.0-20220822230855-b0a4917ee28c
+
+require golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
--- app/main.go
+++ app/main.go
@@ -10,18 +10,14 @@ import (
 )
 
 func main() {
-	var i, j = 1, 2
-	k := utils.Add(i, j)
+	k := utils.Add(1, 2)
 	fmt.Println("Result:", k)
 
-	var strc utils.Nic
 	fmt.Println("Running playground executable:")
 	var n, _ = net.Interfaces()
 	for _, num := range n {
 		fmt.Println(num)
 	}
-	strc.Mac = n[1].HardwareAddr
-	strc.Mtu = n[1].MTU
-	strc.Name = n[1].Name
-	fmt.Println(strc)
+	var addr, _ = n[0].Addrs()
+	fmt.Println("IP:", addr)
 }
